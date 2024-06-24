48464439fb4ae305e877f286eb8cf4de17f312b8
KelvyDavid <kelvydavid.92@gmail.com>
2024-06-24 17:41:16 -0500

    Databases settings
    configuración de la base de datos en el archivo settings.py

--- gestionEventos/settings.py
+++ gestionEventos/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gestionEventos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DataEvents',
+        'USER': 'chat_c3_user',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
