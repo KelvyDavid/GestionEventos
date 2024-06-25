b26f5d66c2dbe894a0436d8367ee388e3301c4bd
KelvyDavid <kelvydavid.92@gmail.com>
2024-06-24 21:14:10 -0500

    Modelado de tablas
    Creación de los modelos para la DB, aun no se realiza el proceso de migración

--- eventsManagement/models.py
+++ eventsManagement/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+
+class Usuarios(AbstractUser):
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(Group, related_name= 'customuser_set')
+    user_permissions = models.ManyToManyField(Permission, related_name= 'customuser_set')
+
+class Eventos(models.Model):
+    user_id = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    eventName = models.TextField()
+    description = models.TextField()
+    ubicacion = models.TextField()
+    timestamp = models.DateTimeField()
+
+    class Meta:
+        ordering= ['timestamp']
+
+class Inscripciones(models.Model):
+    user_id = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Eventos, on_delete=models.CASCADE)
+    reg_date = models.DateTimeField(auto_now_add=True)
--- gestionEventos/settings.py
+++ gestionEventos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App propias
+    'eventsManagement',
 ]
 
 MIDDLEWARE = [
@@ -107,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
@@ -119,9 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'eventsManagement.Usuarios'
