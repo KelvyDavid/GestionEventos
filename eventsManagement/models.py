6713f84bf98c9a8a7950fa0fbd9e0e39253c3696
KelvyDavid <kelvydavid.92@gmail.com>
2024-07-05 18:51:51 -0500

    update model

--- eventsManagement/models.py
+++ eventsManagement/models.py
@@ -15,9 +15,10 @@ class Usuario(AbstractUser):
 class Evento(models.Model):
     nombre_evento = models.CharField(max_length=150)
     descripcion = models.TextField()
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
-    cupos = models.PositiveIntegerField()
+    fecha_evento = models.DateField()
+    #fecha_fin = models.DateField()
+    #cupos = models.PositiveIntegerField()
+    ubicacion= models.CharField(max_length=150)
     estado = models.BooleanField(default=True)
     inscritos = models.ManyToManyField(Usuario, related_name='eventos_inscritos', blank=True)
 
