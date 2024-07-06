54b8c7745b782f2dc96f1341e2bc918d71d7260d
KelvyDavid <kelvydavid.92@gmail.com>
2024-07-06 16:30:04 -0500

    update

--- eventsManagement/models.py
+++ eventsManagement/models.py
@@ -11,14 +11,13 @@ class Usuario(AbstractUser):
 
     rol = models.CharField(max_length=7, choices=ROLES, default='normal')
 
-
 class Evento(models.Model):
     nombre_evento = models.CharField(max_length=150)
     descripcion = models.TextField()
     fecha_evento = models.DateField()
-    ubicacion= models.CharField(max_length=150)
+    ubicacion = models.CharField(max_length=150)
     estado = models.BooleanField(default=True)
     inscritos = models.ManyToManyField(Usuario, related_name='eventos_inscritos', blank=True)
 
     def __str__(self):
-        return self.nombre 
+        return self.nombre_evento
